e9a6415a12232a862bb5e30beb733371591edf08
Patrik Janoušek <patrikjanousek97@gmail.com>
2020-01-23 23:25:01 +0100

    removed unused func

--- vfs/utils.go
+++ vfs/utils.go
@@ -2,10 +2,6 @@ package vfs
 
 import "unsafe"
 
-func AlignNumber(number, multiplier int) int {
-	return number + (multiplier - (number % multiplier))
-}
-
 func InodePtrToVolumePtr(superblock Superblock, ptr InodePtr) VolumePtr {
 	return superblock.InodesStartAddress + VolumePtr(ptr * InodePtr(unsafe.Sizeof(Inode{})))
 }
